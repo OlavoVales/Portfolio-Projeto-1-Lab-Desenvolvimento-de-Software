164f737a9e274253040f5576bf1fe43d4a04eb2a
GabrielReiiss <reislebron@gmail.com>
2025-08-15 15:41:35 -0300

    Adicionado alguns elementos na pagina inicial

--- src/app/components/pagina-inicial/pagina-inicial.ts
+++ src/app/components/pagina-inicial/pagina-inicial.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, signal, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../footer/footer';
+import { IExperiencias } from '../interface/IExperiencias.interface';
 
 @Component({
   selector: 'app-pagina-inicial',
   imports: [RouterModule, Footer],
   templateUrl: './pagina-inicial.html',
-  styleUrl: './pagina-inicial.css'
+  styleUrls: ['./pagina-inicial.css']
 })
-export class PaginaInicial {
+export class PaginaInicial implements AfterViewInit {
 
+  public arrayExperiencias = signal<IExperiencias[]>([
+    {
+      summary: {
+        strong: 'COLOQUEI AQUI O CARGO',
+        p: 'COLOQUE AQUI A EMPRESA | DATA',
+      },
+      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+    },
+    {
+      summary: {
+        strong: 'COLOQUEI AQUI O CARGO',
+        p: 'COLOQUE AQUI A EMPRESA | DATA',
+      },
+      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+    }
+  ]);
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const detailsElements = document.querySelectorAll<HTMLDetailsElement>('.experiencias details');
+    detailsElements.forEach(det => {
+      det.addEventListener('toggle', () => {
+        if (det.open) {
+          setTimeout(() => {
+            det.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+            });
+          }, 200);
+        }
+      });
+    });
+  }
 }
