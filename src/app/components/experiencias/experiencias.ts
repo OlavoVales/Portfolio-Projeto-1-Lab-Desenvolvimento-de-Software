3581de8badc098c38c4501a9c8a7eed80bcecc59
GabrielReiiss <reislebron@gmail.com>
2025-08-15 18:11:53 -0300

    Començando a estrutura da página de experiências

--- src/app/components/experiencias/experiencias.ts
+++ src/app/components/experiencias/experiencias.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../footer/footer';
+import { IExperiencias } from '../interface/IExperiencias.interface';
 
 @Component({
-  selector: 'app-experiencias',
+  selector: 'app-pagina-inicial',
   imports: [RouterModule, Footer],
   templateUrl: './experiencias.html',
   styleUrl: './experiencias.css'
 })
-export class Experiencias {
+export class Experiencias implements AfterViewInit {
 
+  public arrayExperiencias = signal<IExperiencias[]>([
+    {
+      summary: {
+        strong: 'COLOQUEI AQUI O CARGO',
+        p: 'COLOQUE AQUI A EMPRESA | DATA',
+      },
+      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+    },
+    {
+      summary: {
+        strong: 'COLOQUEI AQUI O CARGO',
+        p: 'COLOQUE AQUI A EMPRESA | DATA',
+      },
+      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+    },
+    {
+      summary: {
+        strong: 'COLOQUEI AQUI O CARGO',
+        p: 'COLOQUE AQUI A EMPRESA | DATA',
+      },
+      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+    }
+  ]);
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const detailsElements = document.querySelectorAll<HTMLDetailsElement>('.experiencias details');
+    detailsElements.forEach(det => {
+      det.addEventListener('toggle', () => {
+        if (det.open) {
+          setTimeout(() => {
+            det.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+            });
+          }, 200);
+        }
+      });
+    });
+  }
 }
