153362e880ebe86bcbb565f03f0001c801e32221
OlavoVales <olavovgcs@gmail.com>
2025-08-22 00:28:13 -0300

    adicao da api

--- back/PortfolioAPI/PortfolioAPI/Program.cs
+++ back/PortfolioAPI/PortfolioAPI/Program.cs
@@ -1,11 +1,33 @@
+﻿using PortfolioAPI.Services;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddUserSecrets<Program>();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IContatoService, ContatoService>();
+
+// Configuração de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular"); // ← IMPORTANTE: Deve vir antes de UseAuthorization()
 
 app.UseAuthorization();
 
---
+++ back/PortfolioAPI/PortfolioAPI/Services/ContatoService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PortfolioAPI.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace PortfolioAPI.Services
+{
+    public interface IContatoService
+    {
+        Task EnviarEmail(InformacoesUsuario infosUser);
+    }
+    public class ContatoService: IContatoService
+    {
+        private readonly IConfiguration configuration;
+
+        public ContatoService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task EnviarEmail(InformacoesUsuario infosUser)
+        {
+            var meuEmail = configuration.GetValue<string>("EMAIL_CONFIGURATION:EMAIL");
+            var minhaSenha = configuration.GetValue<string>("EMAIL_CONFIGURATION:PASSWORD");
+            var host = configuration.GetValue<string>("EMAIL_CONFIGURATION:HOST");
+            var port = configuration.GetValue<int>("EMAIL_CONFIGURATION:PORT");
+
+            var smtpClient = new SmtpClient(host, port);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+
+            smtpClient.Credentials = new NetworkCredential(meuEmail, minhaSenha);
+
+            var mensagemCompleta = new MailMessage(meuEmail!, infosUser.Email, infosUser.Assunto, infosUser.Mensagem);
+
+            await smtpClient.SendMailAsync(mensagemCompleta);
+        }
+    }
+}
