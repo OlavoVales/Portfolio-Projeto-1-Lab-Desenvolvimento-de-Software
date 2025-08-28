2c709351aeb8eff0872581068ded294504a56f5e
OlavoVales <olavovgcs@gmail.com>
2025-08-28 20:34:17 -0300

    adicionado arquivos sem erro

---
+++ back/PortfolioAPI/PortfolioAPI/Controllers/ContatoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioAPI.Models;
+using PortfolioAPI.Services;
+
+namespace PortfolioAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly IContatoService contatoService;
+        public ContatoController(IContatoService contatoService) 
+        {
+            this.contatoService = contatoService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EnviarEmail([FromBody] InformacoesUsuario infosUser)
+        {
+            try
+            {
+                Console.WriteLine($"Recebido: {infosUser.Email}, {infosUser.Assunto}");
+                await contatoService.EnviarEmail(infosUser);
+                return Ok(new { message = "Email enviado com sucesso" });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+                return StatusCode(500, new { error = "Erro interno do servidor" });
+            }
+        }
+    }
+}
---
+++ back/PortfolioAPI/PortfolioAPI/Models/InformacoesUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioAPI.Models
+{
+    public class InformacoesUsuario
+    {
+        public string Email { get; set; }
+        public string Assunto { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
