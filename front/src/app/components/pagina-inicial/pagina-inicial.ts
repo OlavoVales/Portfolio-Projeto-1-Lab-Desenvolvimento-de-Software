37c075c60006ec6de6ddd4078b0c3d0f5db3ecff
OlavoVales <olavovgcs@gmail.com>
2025-08-29 19:28:58 -0300

    alteração do dados boilerplate para meus dados pessoais

--- front/src/app/components/pagina-inicial/pagina-inicial.ts
+++ front/src/app/components/pagina-inicial/pagina-inicial.ts
@@ -15,17 +15,17 @@ export class PaginaInicial implements AfterViewInit {
   public arrayExperiencias = signal<IExperiencias[]>([
     {
       summary: {
-        strong: 'COLOQUEI AQUI O CARGO',
-        p: 'COLOQUE AQUI A EMPRESA | DATA',
+        strong: 'Assistente de TI júnior',
+        p: 'Tribunal de Contas do Estado de Minas Gerais | abr de 2025 - atualmente',
       },
-      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+      text: "Participei da atuação no projeto de melhorias no Sistema de Gestão e Administração de Processos (SGAP), utilizando conhecimentos em linguagens como C# e JavaScript, além do framework ASP .NET Core. Trabalhei com banco de dados Oracle e utilizei a ferramenta Visual Studio (versão 10 ou superior) para o desenvolvimento. O projeto foi conduzido com base em metodologias ágeis, como Scrum e Kanban, garantindo maior eficiência e colaboração nas entregas.",
     },
     {
       summary: {
-        strong: 'COLOQUEI AQUI O CARGO',
-        p: 'COLOQUE AQUI A EMPRESA | DATA',
+        strong: 'Estagiário de desenvolvimento',
+        p: 'Tribunal de Contas do Estado de Minas Gerais | mai de 2024 - abr de 2025',
       },
-      text: "COLOQUE AQUI UMA BREVE DESCRIÇÃO DA SUA EXPERIÊNCIA",
+      text: "Manutenção e desenvolvimento de sistemas em modelo MVC, utilizando do framework ASP.NET Core/C#, JavaScript e Oracle Database.",
     }
   ]);
 
